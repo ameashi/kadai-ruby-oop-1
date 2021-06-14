519a08e6fa649c492c01822c0c4b1f4cdb0a665f
ajifurai <furaiaji79@gmail.com>
2021-06-14 09:32:43 +0000

    02

--- team.rb
+++ team.rb
@@ -21,18 +21,18 @@ class Team
 end
 
 # インスタンスの生成と、変数への代入
-Giants = Team.new("Giants", 67, 45, 8)
-Tigers = Team.new("Tigers", 60, 53, 7)
-Dragons = Team.new("Dragons", 60, 55, 5)
-BayStars = Team.new("BayStars", 56, 58, 6)
-Carp = Team.new("Carp", 52, 56, 12)
-Swallows = Team.new("Swallows", 41, 69, 10)
+giants = Team.new("Giants", 67, 45, 8)
+tigers = Team.new("Tigers", 60, 53, 7)
+dragons = Team.new("Dragons", 60, 55, 5)
+baystars = Team.new("BayStars", 56, 58, 6)
+carp = Team.new("Carp", 52, 56, 12)
+swallows = Team.new("Swallows", 41, 69, 10)
 
 # インスタンスの使用
-Giants.show_team_result
+giants.show_team_result
 # puts"#{Giants.name}の2020年の成績は#{Giants.win}勝 #{Giants.lose}敗 #{Giants.draw}分、勝率は#{Giants.calc_win_rate}です。"
-Tigers.show_team_result
-Dragons.show_team_result
-BayStars.show_team_result
-Carp.show_team_result
-Swallows.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+baystars.show_team_result
+carp.show_team_result
+swallows.show_team_result
